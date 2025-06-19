393e8aaff980b77cf96f1d9233e493a23b2b3e77
dzzk-r <danielworkload@gmail.com>
2025-06-19 12:52:43 +0000

    PWA-like approach

---
+++ docs/service-worker.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = "oref-cache-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./css/main.css",
+  "./js/main.js",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
