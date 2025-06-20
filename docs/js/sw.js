6ddbdc53ab5500fa64655758a2f3a7969055a2b5
dzzk-r <danielworkload@gmail.com>
2025-06-20 07:40:44 +0000

    v4 stabilization

---
+++ docs/js/sw.js
@@ -0,0 +1,51 @@
+// File: docs/js/sw.js
+
+const CACHE_NAME = "oref-info-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./css/main.css",
+  "./OREF-offline-v1.apk",
+  "./slides.pdf",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "./manifest.json"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const responseClone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseClone);
+        });
+        return response;
+      })
+      .catch(() => {
+        return caches.match(event.request);
+      })
+  );
+});
